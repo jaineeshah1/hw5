d18248761449c1d6bf819d2395dac2cf7e096a75
Jainee Shah <jaineesh@usc.edu>
2022-11-29 06:29:49 +0000

    adding pdfs

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,7 +19,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleInner(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, std::vector<int>> map_workers,
+    unsigned int worker,
+    unsigned int day
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -30,14 +38,104 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
+    unsigned int number_days = avail.size();
+    unsigned int number_workers = avail[0].size();
+
+    std::map<int, std::vector<int>> map_workers;
+
+    for (unsigned int day = 0; day < number_days; ++day)
+    {
+        sched.push_back(std::vector<Worker_T>(dailyNeed, -1));
     }
-    sched.clear();
-    // Add your code below
+    for (unsigned int worker = 0; worker < number_workers; ++worker)
+    {
+        map_workers[worker] = std::vector<int>();
+    }
 
-
-
-
+    return scheduleInner(
+        avail,
+        dailyNeed,
+        maxShifts,
+        sched,
+        map_workers,
+        0,
+        0
+    );
 }
 
+bool scheduleInner(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, std::vector<int>> map_workers,
+    unsigned int worker,
+    unsigned int day
+)
+{
+    // iterate through all worker options for given day
+    for (unsigned int i = 0; i < map_workers.size(); ++i)
+    {
+        // check if worker is available on that day
+        bool bAvailable = avail[day][i];
+
+        // if i in sched[day]
+        if (std::count(sched[day].begin(), sched[day].end(), i)) {
+            continue;
+        }
+
+        // check if worker has exceeded maxShifts
+        bool bMoreShiftsAllowed = map_workers[i].size() < maxShifts;
+
+        if (bAvailable && bMoreShiftsAllowed)
+        {
+            sched[day][worker] = i;
+            map_workers[i].push_back(day);
+
+            // recurse if daily need is not met
+            bool bDailyNeedNotMet = (worker + 1) < dailyNeed;
+            if (bDailyNeedNotMet) { // fill in row for day
+                bool worked = scheduleInner(
+                    avail,
+                    dailyNeed,
+                    maxShifts,
+                    sched,
+                    map_workers,
+                    worker + 1,
+                    day
+                );
+                if (!worked) {
+                    // backtrack
+                    sched[day][worker] = -1;
+                    map_workers[i].pop_back();
+                }
+                else {
+                    return true;
+                }
+            } 
+            else if (day < (avail.size() - 1)) { // move on to next day
+                bool worked = scheduleInner( 
+                    avail,
+                    dailyNeed,
+                    maxShifts,
+                    sched,
+                    map_workers,
+                    0,
+                    day + 1
+                );
+                if (!worked) {
+                    // backtrack
+                    sched[day][worker] = -1;
+                    map_workers[i].pop_back();
+                }
+                else {
+                    return true;
+                }
+            }
+            else { // at last day and worker
+                return true;
+            }
+        }
+    }
+    return false;
+}
